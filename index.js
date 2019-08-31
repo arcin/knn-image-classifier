c1c7f721e31ab7ace270451cff84f62f3f1b4e14
Charles Arciniega <charlesarcin@gmail.com>
2019-08-30 19:52:30 -0700

    Extend image classifier to support three custom categories.
    - imported TensorFlow KNN.
    - Used tensorflow knn model as a classifier instead of just mobilenet.
    - Added three buttons to classification categories: A, B, and C.
      - When clicking the buttons, we grab an activation from mobilenet and
        add it to KNN's dataset via `KNNClassifier::addExample`
    - Render predictions from KNN classifier instead of MobileNet.
      - Whenever a frame is grabbed from the webcam, we grab its activation
      from Mobilenet and feed it into the knn classifier to get its
      prediction.
      - The predictions are based on the examples added to the A, B, or C
        categories.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
 let net;
 
 async function app() {
@@ -10,20 +11,37 @@ async function app() {
 
   await setupWebcam();
 
-  while (true) {
-    const result = await net.classify(webcamElement);
+  // Associates webcam image with specific class index
+  const addExample = classId => {
+    // Get intermediate activation of MobileNet 'conv_preds';
+    const activation = net.infer(webcamElement, 'conv_preds');
 
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
-    `;
+    // Pass activation to classifier
+    classifier.addExample(activation, classId);
+  }
+
+  // When clicking a button, we add an example to the corresponding classifier.
+  document.getElementById('class-a').addEventListener('click', () => addExample(0))
+  document.getElementById('class-b').addEventListener('click', () => addExample(1))
+  document.getElementById('class-c').addEventListener('click', () => addExample(2))
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      // Get activation for webcam from mobilenet.
+      const activation = net.infer(webcamElement, 'conv_preds');
+
+      // Get the most likely class and confidences from the classifier module.
+      const result = await classifier.predictClass(activation);
+      const classes = ['A', 'B', 'C']
+
+      document.getElementById('console').innerText = `
+        prediction: ${classes[result.classIndex]}\n
+        probability: ${result.confidences[result.classIndex]}
+      `;
+    }
 
     await tf.nextFrame();
   }
-  // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result);
 }
 
 async function setupWebcam() {
